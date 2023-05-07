a165d5757492afc521908801747ebed1d2b1f24b
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2023-05-08 01:10:03 +0530

    added code

--- 03ToDoApp_v1/script/script.js
+++ 03ToDoApp_v1/script/script.js
@@ -1,5 +1,25 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
     alert("Please Enter a Task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+    <div class="task">
+      <span id="taskname">
+      ${document.querySelector("#newtask input").value}
+      </span>
+        <button class="delete">
+          <i class="fa-solid fa-trash-can"></i>
+        </button>
+    </div>`;
+
+    document.querySelector("#newtask input").value = "";
+
+    var current_tasks = document.querySelectorAll(".delete");
+    console.log(typeof current_tasks);
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
   }
 };
